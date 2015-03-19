a727e1c9d4768201b98eb80976254a809c3c49a4
Shadi Iskandar <shadi13@meet.mit.edu>
2015-03-19 19:59:33 +0200

    sign up

--- project/app/urls.py
+++ project/app/urls.py
@@ -6,4 +6,5 @@ from app import views
 urlpatterns = patterns('',
     url(r'^home/$', views.home, name='home'),
     url(r'^$', views.index, name='index'),
+    url(r'^signup/$', views.signup, name='signup'),
 )
--- project/app/views.py
+++ project/app/views.py
@@ -5,3 +5,5 @@ def index(request):
 	return render(request, 'app/index.html', {})
 def home(request):
 	return render(request, 'app/home.html', {})
+def signup(request):
+	return render(request, 'app/signup.html', {})
